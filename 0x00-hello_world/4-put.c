bf5df9e58d8e5651978f0d0e04da71fbe0a66036
dekspek <themindbeams@gmail.com>
2022-07-03 05:30:24 -0700

    Task 4

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (sucess)
+ * main -Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	char str[] - "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
 }
