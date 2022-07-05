53e934b2747915197c024a8f13d6c24f688dcecb
dekspek <themindbeams@gmail.com>
2022-07-05 15:55:28 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
-#include "main.h" 
- 
-/** 
-  * main - print _putcher 
-  * 
-  * Return: Always 0 
-  */ 
-int main(void) 
-{ 
-	_putchar('_'); 
-	_putchar('p'); 
-	_putchar('u'); 
-	_putchar('t'); 
-	_putchar('c'); 
-	_putchar('h'); 
-	_putchar('a'); 
-	_putchar('r'); 
-	_putchar('\n'); 
+#include "main.h"
 
-	return (0); 
+/**
+ * main - function to print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
