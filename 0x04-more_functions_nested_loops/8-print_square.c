37a5943dd251b022f6d6cb9ab0263da5bc111342
dekspek <themindbeams@gmail.com>
2022-07-08 14:40:05 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,44 +1,27 @@
 #include "main.h"
 
-
 /**
- * print_square - print square
- * @size: number to print
- * Return: 0
+ * print_square -  checks for checks for a digit (0 through 9).
+ * @size: size -  Variable
+ * Return: Always 0.
  */
-
-
-
-void print_square(int
+void print_square(int size)
 {
-	int first, second;
+	int x, y;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-
-		_putchar('\n');
-
+	for (x = 1; x <= size; x++)
+	{
+	for (y = 1; y <= size; y++)
+	{
+	_putchar('#');
 	}
-
+	_putchar('\n');
+	}
+	}
 	else
 	{
-		for (first = 0; first < size; first++)
-
-		{
-			for (second = 0; second < size; second++)
-			{
-
-				_putchar('#');
-
-			}
-			if (first == size - 1)
-
-				continue;
-
-			_putchar('\n');
-
-		}
-
-		_putchar('\n');
-
+	_putchar('\n');
 	}
+}
