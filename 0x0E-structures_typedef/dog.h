6bb882422ca032f6563c95dde878c7b999d0c501
dekspek <themindbeams@gmail.com>
2022-08-02 06:21:00 -0700

    TASK 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef strucdt dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 voif print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char * owner);
