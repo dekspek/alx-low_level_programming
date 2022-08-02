0a998269c054a3645dafcdd35df14412b942584e
dekspek <themindbeams@gmail.com>
2022-08-02 16:33:58 -0700

    All task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a file name and new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
